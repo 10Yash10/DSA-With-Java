c8e267ecdbd1c08e4fa6c92c711306cc728ca5f8
10Yash10 <91713101+10Yash10@users.noreply.github.com>
2024-07-29 17:28:12 +0530

    Update frequencyOfNumber.java
    some changes made i.e removed for loop that checks every elements is not equal to the given number. Instead a simple things is checked i.e. if the given number is greater than or equal to the length of frequency array then the result would be zero else the result would be given by that index value of frequency.

--- frequencyOfNumber.java
+++ frequencyOfNumber.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 class Test{
     public static void main(String [] args){
         Integer[] arr = {1, 1, 2, 2, 2, 2, 3};
-        Integer N = 7, X = 5;
+        Integer N = 7, X = 2;
 
         Integer greatest = arr[N - 1];
         Integer [] freq = new Integer[greatest + 1];
@@ -11,12 +11,16 @@ class Test{
         for(Integer i : arr){
             freq[i]++;
         }   
-        for(Integer i : arr){
-            if(X != i){
-                System.out.println("0");
-                return ;
-            }
+        // for(Integer i : arr){
+        //     if(X != i){
+        //         System.out.println("0");
+        //         return ;
+        //     }
+        // }
+        if(X>=freq.length){
+            System.out.println("Max frequency: 0");
+        }else{
+            System.out.println("\nMax frequency: " + freq[X]);
         }
-        System.out.println("Max frequency: " + freq[X]);
     }
 }
