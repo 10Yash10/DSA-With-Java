df767eb9994b716a59cc39d6228f079dee123556
10Yash10 <91713101+10Yash10@users.noreply.github.com>
2024-07-29 17:21:08 +0530

    Create arrangeFromNegativeToPositive.java
    this is to arrange all the numbers in ascending order. Here all the negative numbers are places firstly and then all the positive numbers are places after the negative numbers are placed

---
+++ arrangeFromNegativeToPositive.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class arrangeFromNegativeToPositive {
+    int arr[] = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
+    Arrays.sort(arr);
+
+    for(int i: arr){
+        System.out.print(i + " ");
+    }
+}
