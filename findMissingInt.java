8fd479b936983e3af2ece1350899425311fd2149
10Yash10 <91713101+10Yash10@users.noreply.github.com>
2024-08-05 14:51:06 +0530

    Update findMissingInt.java
    code writing is done.

--- findMissingInt.java
+++ findMissingInt.java
@@ -1,3 +1,20 @@
 public class findMissingInt {
-    
+
+    static void find(int [] arr, int n){
+        int count = 1;
+        for(int i = 0; i < n ; i++){
+            if(count != arr[i]){
+                System.out.print("Missing Value: " + count);
+                break;
+            }
+            count++;
+        }
+    }
+
+    public static void main(String [] args){
+        int arr[] = { 1,2,3,4,6,7 };
+        int n = arr.length;
+        find(arr, n);
+        
+    }
 }
